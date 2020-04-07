d5358c0adc255f851abaea14d41257e49f424849
antoniotrkdz <antoniotrkdz@gmail.com>
2020-04-07 19:00:57 +0200

    Major update of the content. Add recent work experience.

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,27 +1,27 @@
-console.log('This would be the main JS file.');
+console.log("This would be the main JS file.");
 
 //--Drawing header and footer patterns using Trianglify library--------------------
 
 function draw() {
-  var header = document.getElementById('header');
-  var footer = document.getElementById('footer');
+  var header = document.getElementById("header");
+  var footer = document.getElementById("footer");
   var colorArray = [
-    '#ffffff',
-    '#ffffff',
-    '#ffffff',
-    '#ffffff',
-    '#ffffff',
-    '#f0f0f0',
-    '#d9d9d9',
-    '#bdbdbd',
-    '#969696',
-    '#737373',
-    '#525252',
-    '#252525',
-    '#000000',
-    '#000000',
-    '#000000',
-    '#000000',
+    "#ffffff",
+    "#ffffff",
+    "#ffffff",
+    "#ffffff",
+    "#ffffff",
+    "#f0f0f0",
+    "#d9d9d9",
+    "#bdbdbd",
+    "#969696",
+    "#737373",
+    "#525252",
+    "#252525",
+    "#000000",
+    "#000000",
+    "#000000",
+    "#000000",
   ];
 
   var hpattern = Trianglify({
@@ -30,7 +30,7 @@ function draw() {
     height: header.offsetHeight,
     variance: 1,
     x_colors: colorArray,
-    y_colors: ['#252525', '#252525'],
+    y_colors: ["#252525", "#252525"],
     stroke_width: 1,
     cell_size: 30,
   });
@@ -41,17 +41,17 @@ function draw() {
     height: footer.offsetHeight,
     variance: 1,
     x_colors: colorArray.reverse(),
-    y_colors: ['#252525', '#252525'],
+    y_colors: ["#252525", "#252525"],
     stroke_width: 1,
     cell_size: 30,
   });
 
   //These are to prevent the function to draw more than 1 pattern
-  var hsvg = header.getElementsByTagName('svg');
+  var hsvg = header.getElementsByTagName("svg");
   if (hsvg.length > 0) header.removeChild(hsvg[0]);
   header.appendChild(hpattern.svg());
 
-  var fsvg = footer.getElementsByTagName('svg');
+  var fsvg = footer.getElementsByTagName("svg");
   if (fsvg.length > 0) footer.removeChild(fsvg[0]);
   footer.appendChild(fpattern.svg());
 }
@@ -60,13 +60,13 @@ function draw() {
 
 function randomPath() {
   //Choses a svg element at random.
-  var svgNodes = header.getElementsByTagName('svg')[0].childNodes;
+  var svgNodes = header.getElementsByTagName("svg")[0].childNodes;
   var n = Math.floor(Math.random() * svgNodes.length);
   return svgNodes[n];
 }
 
 function randomPathsArray() {
-  var svgNodes = header.getElementsByTagName('svg')[0].childNodes;
+  var svgNodes = header.getElementsByTagName("svg")[0].childNodes;
   var r = Math.floor(Math.random() * 10);
   var arr = [];
 
@@ -88,9 +88,9 @@ function randomTime(min, max) {
 
 function blink(path, arr) {
   //(Re)fills the path, waits a bit and removes the fill (back to initial css rule).
-  path.style.fill = path.getAttribute('fill');
-  setTimeout(function() {
-    path.style.fill = 'none';
+  path.style.fill = path.getAttribute("fill");
+  setTimeout(function () {
+    path.style.fill = "none";
   }, randomTime(250, 600));
 }
 
@@ -115,14 +115,14 @@ function shuffle(array) {
 }
 
 function randomColors() {
-  var colors = ['#09B3CA', '#F19F19', '#B5CF5D', '#BB1881'];
+  var colors = ["#09B3CA", "#F19F19", "#B5CF5D", "#BB1881"];
   colors = shuffle(colors); //shuffles the colors
 
   var elements = {
-    blue: document.querySelectorAll('.blue'),
-    orange: document.querySelectorAll('.orange'),
-    purple: document.querySelectorAll('.purple'),
-    green: document.querySelectorAll('.green'),
+    tbd0: document.querySelectorAll(".tbd0"),
+    tbd1: document.querySelectorAll(".tbd1"),
+    tbd2: document.querySelectorAll(".tbd2"),
+    tbd3: document.querySelectorAll(".tbd3"),
   };
 
   var j = 0;
@@ -130,7 +130,7 @@ function randomColors() {
   for (var i in elements) {
     //cicles through the elements to be colored and applies a color from the shuffled array.
     for (e = 0; e < elements[i].length; e++) {
-      if (elements[i][e].className.indexOf('box') === -1) {
+      if (elements[i][e].className.indexOf("box") === -1) {
         elements[i][e].style.color = colors[j];
       } else elements[i][e].style.borderColor = colors[j];
     }
@@ -149,48 +149,48 @@ function randomColors() {
 draw(); // draws the triangles header and footer.
 randomColors();
 //window.onresize = function() { draw() } //Redraws the svg(s) on resize for responsive design (two ways of doing the same thing, just pick one):
-window.addEventListener('resize', draw);
+window.addEventListener("resize", draw);
 
-var noHover = setInterval(function() {
+var noHover = setInterval(function () {
   blink(randomPath());
 }, randomTime(200, 600));
 var hover;
 
-document.getElementById('myName').onmouseenter = function() {
+document.getElementById("myName").onmouseenter = function () {
   //On entering the name <div> switch to fill arrays of paths.
   clearInterval(noHover);
-  hover = setInterval(function() {
+  hover = setInterval(function () {
     randomPathsArray();
   }, randomTime(200, 600));
 };
 
-document.getElementById('myName').onmouseleave = function() {
+document.getElementById("myName").onmouseleave = function () {
   //On exiting the name <div> return to the previous fill pattern.
   clearInterval(hover);
-  noHover = setInterval(function() {
+  noHover = setInterval(function () {
     blink(randomPath());
   }, randomTime(200, 600));
 };
 
-var arr = document.getElementsByTagName('a');
+var arr = document.getElementsByTagName("a");
 
 arr = Array.from(arr);
 
-arr.forEach(function(e) {
-  e.onmouseenter = function() {
+arr.forEach(function (e) {
+  e.onmouseenter = function () {
     //On entering the name <div> switch to fill arrays of paths.
     clearInterval(noHover);
-    hover = setInterval(function() {
+    hover = setInterval(function () {
       randomPathsArray();
     }, randomTime(200, 600));
   };
 });
 
-arr.forEach(function(e) {
-  e.onmouseleave = function() {
+arr.forEach(function (e) {
+  e.onmouseleave = function () {
     //On exiting the name <div> return to the previous fill pattern.
     clearInterval(hover);
-    noHover = setInterval(function() {
+    noHover = setInterval(function () {
       blink(randomPath());
     }, randomTime(200, 600));
   };
